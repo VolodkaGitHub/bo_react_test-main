dfa1b0c3514bd4cea299404a42770b7b8db12c84
Volodka <vzh.volodya@gmail.com>
2025-07-15 18:42:57 +0300

    feature/main-image-update

--- src/components/pages/Main/index.jsx
+++ src/components/pages/Main/index.jsx
@@ -57,7 +57,8 @@ export const Main = () => {
         </title>
       </Helmet>
       <section className="hero">
-        <div className="wrapper wrapper__first-section">
+        <div className="hero__first-section-wrapper">
+          <div className="wrapper wrapper__first-section">
           <div className="hero__wrapper">
             <h1 className="hero__title">{__("main.hero.title")}</h1>
             <p className="hero__subtitle">{__("main.hero.subtitle")}</p>
@@ -68,228 +69,233 @@ export const Main = () => {
               {__("main.hero.button")}
             </div>
           </div>
-        </div>
-        <div className="hero__schedule">
-          <img src={HeroBg} />
+          </div>
+          <div className="hero__schedule">
+            <img src={HeroBg} />
+          </div>
         </div>
       </section>
       <section className="condition">
-        <img
-          src={bgTradeImage}
-          alt="trade"
-          className="condition__bg"
-          aria-hidden="true"
-        />
-        <div className="wrapper">
-          <header className="condition__header">
-            <h2 className="condition__title section-title">
-              {__("main.conditions.title")}
-            </h2>
-          </header>
-          <Grid container spacing={2}>
-            <Grid item sx={{ width: { xs: "100%", lg: "27.8%" } }}>
-              <article className="condition-card">
-                <h3 className="condition-card__title">$5</h3>
-                <p className="condition-card__text">
-                  {__("main.conditions.i-1")}
-                </p>
-              </article>
+        <div className="condition__wrapper">
+          <img
+            src={bgTradeImage}
+            alt="trade"
+            className="condition__bg"
+            aria-hidden="true"
+          />
+          <div className="wrapper">
+            <header className="condition__header">
+              <h2 className="condition__title section-title">
+                {__("main.conditions.title")}
+              </h2>
+            </header>
+            <Grid container spacing={2}>
+              <Grid item sx={{ width: { xs: "100%", lg: "27.8%" } }}>
+                <article className="condition-card">
+                  <h3 className="condition-card__title">$5</h3>
+                  <p className="condition-card__text">
+                    {__("main.conditions.i-1")}
+                  </p>
+                </article>
+              </Grid>
+              <Grid item sx={{ width: { xs: "100%", lg: "23.3%" } }}>
+                <article className="condition-card">
+                  <h3 className="condition-card__title">$1</h3>
+                  <p className="condition-card__text">
+                    {__("main.conditions.i-2")}
+                  </p>
+                </article>
+              </Grid>
+              <Grid item sx={{ width: { xs: "100%", lg: "48.8%" } }}>
+                <article className="condition-card condition-card--bg">
+                  <h3 className="condition-card__title">$10 000</h3>
+                  <p className="condition-card__text">
+                    {__("main.conditions.i-3")}
+                  </p>
+                </article>
+              </Grid>
+              <Grid item sx={{ width: { xs: "100%", lg: "19.5%" } }}>
+                <article className="condition-card">
+                  <h3 className="condition-card__title">50+</h3>
+                  <p className="condition-card__text">
+                    {__("main.conditions.i-4")}
+                  </p>
+                </article>
+              </Grid>
+              <Grid item sx={{ width: { xs: "100%", lg: "54.1%" } }}>
+                <article className="condition-card condition-card--bg">
+                  <h3 className="condition-card__title">0%</h3>
+                  <p className="condition-card__text">
+                    {__("main.conditions.i-5")}
+                  </p>
+                </article>
+              </Grid>
+              <Grid item sx={{ width: { xs: "100%", lg: "26.4%" } }}>
+                <article className="condition-card">
+                  <h3 className="condition-card__title">100+</h3>
+                  <p className="condition-card__text">
+                    {__("main.conditions.i-6")}
+                  </p>
+                </article>
+              </Grid>
             </Grid>
-            <Grid item sx={{ width: { xs: "100%", lg: "23.3%" } }}>
-              <article className="condition-card">
-                <h3 className="condition-card__title">$1</h3>
-                <p className="condition-card__text">
-                  {__("main.conditions.i-2")}
-                </p>
-              </article>
-            </Grid>
-            <Grid item sx={{ width: { xs: "100%", lg: "48.8%" } }}>
-              <article className="condition-card condition-card--bg">
-                <h3 className="condition-card__title">$10 000</h3>
-                <p className="condition-card__text">
-                  {__("main.conditions.i-3")}
-                </p>
-              </article>
-            </Grid>
-            <Grid item sx={{ width: { xs: "100%", lg: "19.5%" } }}>
-              <article className="condition-card">
-                <h3 className="condition-card__title">50+</h3>
-                <p className="condition-card__text">
-                  {__("main.conditions.i-4")}
-                </p>
-              </article>
-            </Grid>
-            <Grid item sx={{ width: { xs: "100%", lg: "54.1%" } }}>
-              <article className="condition-card condition-card--bg">
-                <h3 className="condition-card__title">0%</h3>
-                <p className="condition-card__text">
-                  {__("main.conditions.i-5")}
-                </p>
-              </article>
-            </Grid>
-            <Grid item sx={{ width: { xs: "100%", lg: "26.4%" } }}>
-              <article className="condition-card">
-                <h3 className="condition-card__title">100+</h3>
-                <p className="condition-card__text">
-                  {__("main.conditions.i-6")}
-                </p>
-              </article>
-            </Grid>
-          </Grid>
-          <div className="feature-card-container">
-            <div className="feature-card">
-              <div className="feature-card__bg-blur" />
-              <div className="feature-card__description">
-                <div className="feature-card__imgGroup">
-                  <img src={Rectangle} alt="" />
-                  <img src={Logo} alt="" />
-                  <img src={YellowArrowLeft} alt="" />
+            <div className="feature-card-container">
+              <div className="feature-card">
+                <div className="feature-card__bg-blur" />
+                <div className="feature-card__description">
+                  <div className="feature-card__imgGroup">
+                    <img src={Rectangle} alt="" />
+                    <img src={Logo} alt="" />
+                    <img src={YellowArrowLeft} alt="" />
+                  </div>
+                  <h3 className="feature-card__title">
+                    {__("main.feature-card.i-1")}
+                  </h3>
+                  <p className="feature-card__text">
+                    {__("main.feature-card.i-1.text.i-1")}
+                  </p>
+                  <p className="feature-card__text">
+                    {__("main.feature-card.i-1.text.i-2")}
+                  </p>
+                  <p className="feature-card__text">
+                    {__("main.feature-card.i-1.text.i-3")}
+                  </p>
                 </div>
-                <h3 className="feature-card__title">
-                  {__("main.feature-card.i-1")}
-                </h3>
-                <p className="feature-card__text">
-                  {__("main.feature-card.i-1.text.i-1")}
-                </p>
-                <p className="feature-card__text">
-                  {__("main.feature-card.i-1.text.i-2")}
-                </p>
-                <p className="feature-card__text">
-                  {__("main.feature-card.i-1.text.i-3")}
-                </p>
+                <div className="feature-card__shedule-container">
+                  <img src={FeatureCardShedule} alt="shedule" />
+                </div>
               </div>
-              <div className="feature-card__shedule-container">
-                <img src={FeatureCardShedule} alt="shedule" />
+              <div className="feature-card">
+                <div className="feature-card__bg-blur" />
+                <div className="feature-card__description">
+                  <div className="feature-card__imgGroup">
+                    <img src={Rectangle} alt="" />
+                    <img src={Logo} alt="" />
+                    <img src={Like} alt="" />
+                  </div>
+                  <h3 className="feature-card__title">
+                    {__("main.feature-card.i-2")}
+                  </h3>
+                  <p className="feature-card__text">
+                    {__("main.feature-card.i-2.text.i-1")}
+                  </p>
+                  <p className="feature-card__text">
+                    Some text about features in web app
+                  </p>
+                  <p className="feature-card__text">
+                    Some text about features in web app
+                  </p>
+                </div>
+                <div className="feature-card__shedule-container">
+                  <img src={FeatureCardShedule} alt="shedule" />
+                </div>
+              </div>
+              <div className="feature-card">
+                <div className="feature-card__bg-blur" />
+                <div className="feature-card__description">
+                  <div className="feature-card__imgGroup">
+                    <img src={Rectangle} alt="" />
+                    <img src={Logo} alt="" />
+                    <img src={UniversityCap} alt="" />
+                  </div>
+                  <h3 className="feature-card__title">
+                    {__("main.feature-card.i-3")}
+                  </h3>
+                  <p className="feature-card__text">
+                    {__("main.feature-card.i-3.text.i-1")}
+                  </p>
+                  <p className="feature-card__text">
+                    {__("main.feature-card.i-3.text.i-2")}
+                  </p>
+                  <p className="feature-card__text">
+                    Some text about features in web app
+                  </p>
+                </div>
+                <div className="feature-card__shedule-container">
+                  <img src={FeatureCardShedule} alt="shedule" />
+                </div>
               </div>
             </div>
-            <div className="feature-card">
-              <div className="feature-card__bg-blur" />
-              <div className="feature-card__description">
-                <div className="feature-card__imgGroup">
-                  <img src={Rectangle} alt="" />
-                  <img src={Logo} alt="" />
-                  <img src={Like} alt="" />
-                </div>
-                <h3 className="feature-card__title">
-                  {__("main.feature-card.i-2")}
-                </h3>
-                <p className="feature-card__text">
-                  {__("main.feature-card.i-2.text.i-1")}
-                </p>
-                <p className="feature-card__text">
-                  Some text about features in web app
-                </p>
-                <p className="feature-card__text">
-                  Some text about features in web app
-                </p>
+            <div className="start-action-container">
+              <h4 className="start-action-container__title">
+                {__("main.action.text")}
+              </h4>
+              <div
+                onClick={() => handleSignUpToggle(true)}
+                className="btn btn--middle btn--orange"
+              >
+                {__("main.conditions.button")}
               </div>
-              <div className="feature-card__shedule-container">
-                <img src={FeatureCardShedule} alt="shedule" />
-              </div>
-            </div>
-            <div className="feature-card">
-              <div className="feature-card__bg-blur" />
-              <div className="feature-card__description">
-                <div className="feature-card__imgGroup">
-                  <img src={Rectangle} alt="" />
-                  <img src={Logo} alt="" />
-                  <img src={UniversityCap} alt="" />
-                </div>
-                <h3 className="feature-card__title">
-                  {__("main.feature-card.i-3")}
-                </h3>
-                <p className="feature-card__text">
-                  {__("main.feature-card.i-3.text.i-1")}
-                </p>
-                <p className="feature-card__text">
-                  {__("main.feature-card.i-3.text.i-2")}
-                </p>
-                <p className="feature-card__text">
-                  Some text about features in web app
-                </p>
-              </div>
-              <div className="feature-card__shedule-container">
-                <img src={FeatureCardShedule} alt="shedule" />
-              </div>
-            </div>
-          </div>
-          <div className="start-action-container">
-            <h4 className="start-action-container__title">
-              {__("main.action.text")}
-            </h4>
-            <div
-              onClick={() => handleSignUpToggle(true)}
-              className="btn btn--middle btn--orange"
-            >
-              {__("main.conditions.button")}
             </div>
           </div>
         </div>
       </section>
       <section className="reviews-registration-section">
-        <img
-          src={WhiteLogo}
-          className="reviews-registration-section__bg-logo"
-          alt=""
-        />
-        <div className="wrapper">
-          <div className="text-container">
-            <p className="reviews-registration-section__title">
-              {__("main.reviews-registration-section.title.i-1")}
-            </p>
-            <p className="reviews-registration-section__text">
-              {" "}
-              {__("main.reviews-registration-section.subtitle.i-1")}
-            </p>
-          </div>
-        </div>
-        <FeedbackSlider />
-        <div className="wrapper">
-          <div className="text-container">
-            <p className="reviews-registration-section__title">
-              {__("main.reviews-registration-section.title.i-1")}
-            </p>
-            <p className="reviews-registration-section__text">
-              {" "}
-              {__("main.reviews-registration-section.subtitle.i-2")}
-            </p>
-          </div>
-          <div className="step-card-container">
-            <div className="step-card">
-              <img src={Shield} alt="" />
-              <h4 className="step-card__title">
-                {__("main.reviews-registration-section.step-card-i-1.title")}
-              </h4>
-              <p className="step-card__text">
-                {__("main.reviews-registration-section.step-card-i-1.text")}
+        <div className="reviews-registration-section__wrapper">
+          <img
+            src={WhiteLogo}
+            className="reviews-registration-section__bg-logo"
+            alt=""
+          />
+          <div className="wrapper">
+            <div className="text-container">
+              <p className="reviews-registration-section__title">
+                {__("main.reviews-registration-section.title.i-1")}
               </p>
-            </div>
-            <img src={ArrowRight} className="step-card-container__arrow" />
-            <div className="step-card">
-              <img src={Coins} alt="" />
-              <h4 className="step-card__title">
-                {__("main.reviews-registration-section.step-card-i-2.title")}
-              </h4>
-              <p className="step-card__text">
-                {__("main.reviews-registration-section.step-card-i-2.text")}
-              </p>
-            </div>
-            <img src={ArrowRight} className="step-card-container__arrow" />
-            <div className="step-card">
-              <img src={UpShedule} alt="" />
-              <h4 className="step-card__title">
-                {__("main.reviews-registration-section.step-card-i-2.title")}
-              </h4>
-              <p className="step-card__text">
-                {__("main.reviews-registration-section.step-card-i-2.text")}
+              <p className="reviews-registration-section__text">
+                {" "}
+                {__("main.reviews-registration-section.subtitle.i-1")}
               </p>
             </div>
           </div>
-          <div
-            onClick={() => handleSignUpToggle(true)}
-            className="btn btn--middle btn--orange btn-centered"
-          >
-            {__("main.reviews-registration-section.button")}
+          <FeedbackSlider />
+          <div className="wrapper">
+            <div className="text-container">
+              <p className="reviews-registration-section__title">
+                {__("main.reviews-registration-section.title.i-1")}
+              </p>
+              <p className="reviews-registration-section__text">
+                {" "}
+                {__("main.reviews-registration-section.subtitle.i-2")}
+              </p>
+            </div>
+            <div className="step-card-container">
+              <div className="step-card">
+                <img src={Shield} alt="" />
+                <h4 className="step-card__title">
+                  {__("main.reviews-registration-section.step-card-i-1.title")}
+                </h4>
+                <p className="step-card__text">
+                  {__("main.reviews-registration-section.step-card-i-1.text")}
+                </p>
+              </div>
+              <img src={ArrowRight} className="step-card-container__arrow" />
+              <div className="step-card">
+                <img src={Coins} alt="" />
+                <h4 className="step-card__title">
+                  {__("main.reviews-registration-section.step-card-i-2.title")}
+                </h4>
+                <p className="step-card__text">
+                  {__("main.reviews-registration-section.step-card-i-2.text")}
+                </p>
+              </div>
+              <img src={ArrowRight} className="step-card-container__arrow" />
+              <div className="step-card">
+                <img src={UpShedule} alt="" />
+                <h4 className="step-card__title">
+                  {__("main.reviews-registration-section.step-card-i-2.title")}
+                </h4>
+                <p className="step-card__text">
+                  {__("main.reviews-registration-section.step-card-i-2.text")}
+                </p>
+              </div>
+            </div>
+            <div
+              onClick={() => handleSignUpToggle(true)}
+              className="btn btn--middle btn--orange btn-centered"
+            >
+              {__("main.reviews-registration-section.button")}
+            </div>
           </div>
         </div>
       </section>
