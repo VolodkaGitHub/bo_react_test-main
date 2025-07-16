8fc605f3ed5c5f55d42f8f07645d43eb359f7190
Volodka <vzh.volodya@gmail.com>
2025-07-16 12:03:32 +0300

    bugfix/header-justified

--- src/components/layout/Header/components/Unauthorized.jsx
+++ src/components/layout/Header/components/Unauthorized.jsx
@@ -205,19 +205,6 @@ const Unauthorized = () => {
           <li className="header__nav-item">
             <Button
               type={!isAuthRoute ? "button" : "link"}
-              className="header__signUp header__btn"
-              color="orange"
-              size="mini"
-              {...(!isAuthRoute
-                ? { onClick: () => handleSignUpToggle(true) }
-                : { to: "/auth/register" })}
-            >
-              {__("auth.sign_up")}
-            </Button>
-          </li>
-          <li className="header__nav-item">
-            <Button
-              type={!isAuthRoute ? "button" : "link"}
               className="header__login header__btn"
               color="grey-light"
               size="mini"
@@ -226,6 +213,19 @@ const Unauthorized = () => {
                 : { to: "/auth/login" })}
             >
               {__("auth.log_in")}
+            </Button>
+          </li>
+          <li className="header__nav-item">
+            <Button
+              type={!isAuthRoute ? "button" : "link"}
+              className="header__signUp header__btn"
+              color="orange"
+              size="mini"
+              {...(!isAuthRoute
+                ? { onClick: () => handleSignUpToggle(true) }
+                : { to: "/auth/register" })}
+            >
+              {__("auth.sign_up")}
             </Button>
           </li>
         </ul>
